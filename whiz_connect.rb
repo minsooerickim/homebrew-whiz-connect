3000a3f019f70d116acddbd80617b7e3d7143715
Minsoo Kim <minsooerickim@gmail.com>
2023-03-25 16:02:02 -0700

    :seedling: update sha256

--- whiz_connect.rb
+++ whiz_connect.rb
@@ -6,7 +6,7 @@ class WhizConnect < Formula
   homepage "https://github.com/minsooerickim/WhizConnect"
   url "https://github.com/minsooerickim/whiz-connect/archive/refs/tags/v1.1.0.tar.gz"
   version "1.1.0"
-  sha256 "016c1c5e03cf7d90f991e811c14b8e969ca80938f64f6c075dc6f0bf3b4775a0"
+  sha256 "19340ae10e14fde0eac676b729886d22edecf4f07fa509760923f71865fe6524"
   license "MIT"
   
   def install

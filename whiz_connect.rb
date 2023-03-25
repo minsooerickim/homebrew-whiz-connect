e6ce753f7e7066755ee1e7292ba81a1c15ca5bd0
Minsoo Kim <minsooerickim@gmail.com>
2023-03-25 15:58:39 -0700

    :alien: update desc

--- whiz_connect.rb
+++ whiz_connect.rb
@@ -2,7 +2,7 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class WhizConnect < Formula
-  desc "remove password auth when using SSH on remote servers"
+  desc "remove password auth when using SSH on remote servers with user defined encryption method."
   homepage "https://github.com/minsooerickim/WhizConnect"
   url "https://github.com/minsooerickim/whiz-connect/archive/refs/tags/v1.1.0.tar.gz"
   version "1.1.0"
